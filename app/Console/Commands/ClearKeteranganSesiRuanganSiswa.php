562befdf7f77bc41e7f56c2d8fe4e0c132615c5c
hakibro <sandalkuganti@gmail.com>
2025-12-18 20:51:08 +0700

    update

--- app/Console/Commands/ClearKeteranganSesiRuanganSiswa.php
+++ app/Console/Commands/ClearKeteranganSesiRuanganSiswa.php
@@ -3,7 +3,6 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\DB;
 
 class ClearKeteranganSesiRuanganSiswa extends Command
 {
@@ -26,7 +25,8 @@ class ClearKeteranganSesiRuanganSiswa extends Command
      */
     public function handle()
     {
-        DB::table('sesi_ruangan_siswa')
+        \Log::info('Cron clear keterangan dijalankan');
+        \DB::table('sesi_ruangan_siswa')
             ->whereNotNull('keterangan')
             ->update(['keterangan' => null]);
 
