312eafeced9f3ddb7970364a5a6676dccd3e8665
hakibro <sandalkuganti@gmail.com>
2025-12-18 20:43:24 +0700

    sd

--- routes/console.php
+++ routes/console.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Schedule::command('app:clear-keterangan-sesi-ruangan-siswa')
-    ->dailyAt('20:43');
+    ->dailyAt('20:46');
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
