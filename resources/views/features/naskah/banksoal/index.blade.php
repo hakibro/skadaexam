bc3ae12d588708dcd34ed8b6d6f0992489a05746
hakibro <sandalkuganti@gmail.com>
2026-06-13 06:03:58 +0700

    d

--- resources/views/features/naskah/banksoal/index.blade.php
+++ resources/views/features/naskah/banksoal/index.blade.php
@@ -233,7 +233,7 @@
                                         </span>
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                        {{ $bank->totalSoal }}
+                                        {{ $bank->total_soal }}
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         @php
